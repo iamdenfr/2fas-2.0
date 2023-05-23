c0c6b740f766e3c940d496f54f350fa408d08a7b
Filchakov Denis <103757265+iamdenfr@users.noreply.github.com>
2023-05-24 01:23:31 +0300

    Update smooth.js
    changed algorithm for temreatures to ema

--- server/utils/smooth.js
+++ server/utils/smooth.js
@@ -13,16 +13,28 @@ function smoothWindDirection(currentValue, previousWrites) {
 }
   
 function smoothMinTemperature(currentValue, previousWrites) {
-    const sum = previousWrites.reduce((total, write) => total + write.temp_min, 0);
-    const average = sum / previousWrites.length;
-    const result = (currentValue + average) / 2;
+    const alpha = 0.2; 
+    let ema = previousWrites.length > 0 ? previousWrites[0].temp_min : currentValue;
+  
+    for (let i = 1; i < previousWrites.length; i++) {
+      const prevValue = previousWrites[i].temp_min;
+      ema = alpha * prevValue + (1 - alpha) * ema;
+    }
+  
+    const result = (currentValue + ema) / 2;
     return result.toFixed(2);
 }
   
 function smoothMaxTemperature(currentValue, previousWrites) {
-    const sum = previousWrites.reduce((total, write) => total + write.temp_max, 0);
-    const average = sum / previousWrites.length;
-    const result = (currentValue + average) / 2;
+    const alpha = 0.2; 
+    let ema = previousWrites.length > 0 ? previousWrites[0].temp_max : currentValue;
+
+    for (let i = 1; i < previousWrites.length; i++) {
+      const prevValue = previousWrites[i].temp_max;
+      ema = alpha * prevValue + (1 - alpha) * ema;
+    }
+
+    const result = (currentValue + ema) / 2;
     return result.toFixed(2);
 }
   
