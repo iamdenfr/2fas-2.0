d25f6e0a1b268a925834ba2ea5a6b329a909eddf
Filchakov Denis <103757265+iamdenfr@users.noreply.github.com>
2023-05-25 15:04:24 +0300

    Update fetcher.js
    refactored fetcher.js a bit

--- server/utils/fetcher.js
+++ server/utils/fetcher.js
@@ -9,88 +9,89 @@ const Sequelize = require('sequelize');
 
 async function fetchWeatherDataForCity(cityId) {
     try {
-      const users = await User.findAll({
-        where: {
-          city: cityId,
-          apikey: { [Sequelize.Op.not]: null },
-        },
-      });
-  
-      if (users.length > 0) {
-        const user = users[0];
-        const apiKey = user.apikey;
-  
-        const url = `https://api.openweathermap.org/data/2.5/weather?id=${cityId}&appid=${apiKey}&units=${config.units}`;
-  
-        try {
-          const response = await axios.get(url);
-          if (response.status !== 200) {
-            throw new Error(`Error fetching weather data for City ID ${cityId}: ${response.statusText}`)
-          } else {
-          const weatherData = response.data;
-  
-          const windSpeed = weatherData.wind.speed;
-          const windDirection = weatherData.wind.deg;
-          const minTemperature = weatherData.main.temp_min;
-          const maxTemperature = weatherData.main.temp_max;
-          const datetime = new Date();
-          const humidity = weatherData.main.humidity;
-  
-          const previousWrites = await Api.findAll({
+        const users = await User.findAll({
             where: {
-              city: cityId,
-              date: {
-                [Op.gt]: datetime - 30 * 60 * 1000
-              }
+                city: cityId,
+                apikey: { [Sequelize.Op.not]: null },
             },
-            order: [['date', 'DESC']],
-            limit: 5
-          });
-  
-          const smoothedWindSpeed = smoothWindSpeed(windSpeed, previousWrites);
-          const smoothedMinTemperature = smoothMinTemperature(minTemperature, previousWrites);
-          const smoothedMaxTemperature = smoothMaxTemperature(maxTemperature, previousWrites);
-          const smoothedHumidity = smoothHumidity(humidity, previousWrites);
-  
-          if (previousWrites.length > 0) {
-            await ApiData.create({
-              city: cityId,
-              wind_speed: smoothedWindSpeed,
-              wind_deg: windDirection,
-              temp_min: smoothedMinTemperature,
-              temp_max: smoothedMaxTemperature,
-              date: datetime,
-              humidity: smoothedHumidity,
-            });
-          }
-  
-          if (weatherData.weather.length > 0) {
-            await Api.create({
-              city: cityId,
-              wind_speed: windSpeed,
-              wind_deg: windDirection,
-              temp_min: minTemperature,
-              temp_max: maxTemperature,
-              date: datetime,
-              humidity: humidity,
-            });
-          console.log(`Weather data fetched and saved for City ID ${cityId}`);
-          } else {
-            console.log("No response from API");
-          }
+        });
+
+        if (users.length > 0) {
+            const user = users[0];
+            const apiKey = user.apikey;
+
+            const url = `https://api.openweathermap.org/data/2.5/weather?id=${cityId}&appid=${apiKey}&units=${config.units}`;
+
+            const response = await axios.get(url);
+            if (response.status !== 200) {
+                if (response.status === 401) {
+                    console.error(`Error fetching weather data for City ID ${cityId}: Invalid API key`);
+                } else {
+                throw new Error(`Error fetching weather data for City ID ${cityId}: ${response.statusText}`)
+                }
+            } else {
+                const weatherData = response.data;
+
+                const windSpeed = weatherData.wind.speed;
+                const windDirection = weatherData.wind.deg;
+                const minTemperature = weatherData.main.temp_min;
+                const maxTemperature = weatherData.main.temp_max;
+                const datetime = new Date();
+                const humidity = weatherData.main.humidity;
+
+                const previousWrites = await Api.findAll({
+                    where: {
+                        city: cityId,
+                        date: {
+                            [Op.gt]: datetime - 30 * 60 * 1000
+                        }
+                    },
+                    order: [['date', 'DESC']],
+                    limit: 5
+                });
+
+                const smoothedWindSpeed = smoothWindSpeed(windSpeed, previousWrites);
+                const smoothedMinTemperature = smoothMinTemperature(minTemperature, previousWrites);
+                const smoothedMaxTemperature = smoothMaxTemperature(maxTemperature, previousWrites);
+                const smoothedHumidity = smoothHumidity(humidity, previousWrites);
+
+                if (previousWrites.length > 0) {
+                    await ApiData.create({
+                        city: cityId,
+                        wind_speed: smoothedWindSpeed,
+                        wind_deg: windDirection,
+                        temp_min: smoothedMinTemperature,
+                        temp_max: smoothedMaxTemperature,
+                        date: datetime,
+                        humidity: smoothedHumidity,
+                    });
+                }
+
+                if (weatherData.weather.length > 0) {
+                    await Api.create({
+                        city: cityId,
+                        wind_speed: windSpeed,
+                        wind_deg: windDirection,
+                        temp_min: minTemperature,
+                        temp_max: maxTemperature,
+                        date: datetime,
+                        humidity: humidity,
+                    });
+                console.log(`Weather data fetched and saved for City ID ${cityId}`);
+                }
+            }
+        } else {
+            console.log(`No users found for City ID ${cityId}. You can create an API key on openweather.com and add it in the user profile.`);
         }
-  
-        } catch (error) {
-          console.error(`Error fetching weather data for City ID ${cityId}:`, error);
-        }
-      } else {
-        console.log(`No users found for City ID ${cityId}. You can create an API key on openweather.com and add it in the user profile.`);
-      }
     } catch (error) {
-      console.error(`Error fetching users for City ID ${cityId}:`, error);
+        if (error.code === 'ERR_SOCKET_CONNECTION_TIMEOUT') {
+            console.error(`Error fetching weather data for City ID ${cityId}: Request timeout`);
+        } else {
+            console.error(`Error fetching weather data for City ID ${cityId}:`, error);
+        }
     }
 }
 
 module.exports = {
-    fetchWeatherDataForCity
-}
+  fetchWeatherDataForCity
+};
